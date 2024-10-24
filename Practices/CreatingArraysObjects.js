6f29dd30570ab0f9522f29244f2d1cd1820d67ca
Ekrem <ekrem.karali@bilgiedu.net>
2024-10-24 15:27:38 +0300

    RedditPostDownloader with Py
    Just tried out a Py project to make a video post downloader for Reddit.com

---
+++ Practices/CreatingArraysObjects.js
@@ -0,0 +1,15 @@
+var clothes = []
+clothes.push("T-shirt", "Socks","Jacket","Pant","Skirt")
+clothes.pop()
+clothes.push("Underwear")
+console.log(clothes[2])
+var favCar = {}
+favCar.color = "Red"
+favCar.covertible = true
+console.log(favCar)
+
+
+var a = 5;
+if (typeof a != "number") {
+
+}
